0eec304054b3c7b3caa961f79c709efc177dadd7
Andrei <the.docscloud@gmail.com>
2019-05-11 08:08:18 +0300

    loading image as css url

--- lesson04-webpack/src/app.js
+++ lesson04-webpack/src/app.js
@@ -1,14 +1,4 @@
 import 'css/app.css';
-import Logo from '../img/jg_280_rgb.svg';
 import hw from 'utils/hw';
 
 hw();
-
-const logo = document.getElementById('logo');
-const img = new Image();
-img.src = `build/${Logo}`;
-img.width = 200;
-
-logo.appendChild(img);
-
-console.log(Logo);
