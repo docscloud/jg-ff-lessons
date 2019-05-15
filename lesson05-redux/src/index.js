a1973dc080e498aad075b854a797295690fe37a0
Andrei <the.docscloud@gmail.com>
2019-05-15 17:35:05 +0300

    simplest counter app

---
+++ lesson05-redux/src/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const render = () => {
+  document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
