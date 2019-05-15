7eed1ee6348cca6e2a52582849aaefdebc839acb
Andrei <the.docscloud@gmail.com>
2019-05-15 18:09:42 +0300

    using concat to avoid mutation

--- lesson05-redux/src/index.js
+++ lesson05-redux/src/index.js
@@ -2,8 +2,7 @@ import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
 const addCounter = list => {
-  list.push(0);
-  return list;
+  return list.concat([0]);
 };
 
 const testAddCounter = () => {
