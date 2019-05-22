2b09ade2915fe2dee51d278d03e4c5ed36da4d12
Andrei <the.docscloud@gmail.com>
2019-05-22 20:42:34 +0300

    minimal react app

--- lesson06-react/src/app.js
+++ lesson06-react/src/app.js
@@ -1,28 +1,38 @@
 import 'css/app.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import renderHeading from './components/render-heading';
 import renderList from './components/render-list';
 import renderInput from './components/render-input';
 import renderButton from './components/render-button';
 
 let state = { items: [{ name: 'Test item' }], inputValue: '' };
-export const getState = () => state;
-export const updateState = newState => {
-  state = { ...state, ...newState };
-  render();
-};
 
-const render = () => {
-  // console.log('RENDER', state);
-  const app = document.getElementById('app');
-  app.innerHTML = '';
+class App extends Component {
+  render() {
+    return <div>Proverka svjazi</div>;
+  }
+}
 
-  const input = renderInput();
+render(<App />, document.getElementById('app'));
+// export const getState = () => state;
+// export const updateState = newState => {
+//   state = { ...state, ...newState };
+//   render();
+// };
 
-  [renderHeading(), renderList(), input, renderButton()].forEach(element =>
-    app.appendChild(element)
-  );
+// const render = () => {
+//   // console.log('RENDER', state);
+//   const app = document.getElementById('app');
+//   app.innerHTML = '';
 
-  input.focus();
-};
+//   const input = renderInput();
 
-render();
+//   [renderHeading(), renderList(), input, renderButton()].forEach(element =>
+//     app.appendChild(element)
+//   );
+
+//   input.focus();
+// };
+
+// render();
