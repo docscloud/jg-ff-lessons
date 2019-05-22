f296f49734767e582624e36ac7e852002110da0b
Andrei <the.docscloud@gmail.com>
2019-05-22 21:12:17 +0300

    list item rendering and working remove button

--- lesson06-react/src/app.js
+++ lesson06-react/src/app.js
@@ -1,18 +1,21 @@
 import 'css/app.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import renderHeading from './components/render-heading';
-import renderList from './components/render-list';
-import renderInput from './components/render-input';
-import renderButton from './components/render-button';
+// import renderHeading from './components/render-heading';
+// import renderList from './components/render-list';
+// import renderInput from './components/render-input';
+// import renderButton from './components/render-button';
+import Heading from './components/heading';
+import List from './components/list';
 
-let state = { items: [{ name: 'Test item' }], inputValue: '' };
+// let state = { items: [{ name: 'Test item' }], inputValue: '' };
 
-class App extends Component {
-  render() {
-    return <div>Proverka svjazi</div>;
-  }
-}
+const App = () => (
+  <div>
+    <Heading />
+    <List />
+  </div>
+);
 
 render(<App />, document.getElementById('app'));
 // export const getState = () => state;
