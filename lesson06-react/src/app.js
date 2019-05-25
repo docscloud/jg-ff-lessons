1d204535045a4530553969648eaa2e4ccbcaad49
Andrei <the.docscloud@gmail.com>
2019-05-25 12:58:25 +0300

    collapse with hooks

--- lesson06-react/src/app.js
+++ lesson06-react/src/app.js
@@ -1,5 +1,5 @@
 import 'css/app.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -7,21 +7,37 @@ import Heading from './components/heading';
 import List from './components/list';
 import reducer from './reducer';
 import Input from './components/input';
-import Container from './components/container';
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const App = () => (
-  <Provider store={store}>
-    <Heading />
-    <Container>
-      <List />
-    </Container>
-    <Input />
-  </Provider>
-);
+const App = () => {
+  const [display, setDisplay] = useState(true);
+
+  return (
+    <Provider store={store}>
+      <Heading onClick={() => setDisplay(!display)} />
+      {display ? (
+        <List />
+      ) : (
+        <p
+          style={{
+            height: 30,
+            margin: 20,
+            fontSize: 16,
+            backgroundColor: 'lightblue',
+            textAlign: 'center'
+          }}
+        >
+          OOOPS
+        </p>
+      )}
+
+      <Input />
+    </Provider>
+  );
+};
 
 render(<App />, document.getElementById('app'));
--- lesson06-react/src/components/heading.js
+++ lesson06-react/src/components/heading.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Heading = () => <h1>Todo List</h1>;
+const Heading = ({ onClick }) => <h1 onClick={onClick}>Todo List</h1>;
 
 export default Heading;
